f1f180d59f6064349f431a291fa677eb7684f387
Alp <67732998+alpblkba@users.noreply.github.com>
2026-01-09 23:35:05 +0100

    demo

---
+++ cpp/0001.two-sum/solution.cpp
@@ -0,0 +1,36 @@
+// Created by alpblkba at 2026/01/07 22:17
+// leetgo: 1.4.16
+// https://leetcode.com/problems/two-sum/
+
+#include <bits/stdc++.h>
+#include "LC_IO.h"
+using namespace std;
+
+// @lc code=begin
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+// @lc code=end
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	stringstream out_stream;
+
+	vector<int> nums;
+	LeetCodeIO::scan(cin, nums);
+	int target;
+	LeetCodeIO::scan(cin, target);
+
+	Solution *obj = new Solution();
+	auto res = obj->twoSum(nums, target);
+	LeetCodeIO::print(out_stream, res);
+	cout << "\noutput: " << out_stream.rdbuf() << endl;
+
+	delete obj;
+	return 0;
+}
